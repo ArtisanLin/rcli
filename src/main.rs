7d81e5ba9f7995ddd635ef71bb1d15b16f73454c
Linya <cdthanye32@qq.com>
2024-12-23 22:46:36 +0800

    feat: [参数解析] clap 引入 & 解析参数配置

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::path::Path;
+// rcli csv -i input.csv -o output.json --header -d ','
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+// 下面这些都从 Cargo.toml 中拿
+#[command(name="rcli", version, author, about, long_about = None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    #[command(name = "csv", about = "Show CSV, or convert CSV to JSON")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input_file)]
+    input: String,
+
+    // #[arg(short, long, default_value_t = String::from("output.json"))] // "output.json".into()
+    #[arg(short, long, default_value = "output.json")] // "output.json".into()
+    output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+
+    // header 以 h 开头，和 help 冲突，可以不使用short，将其规避
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+    println!("{:?}", opts);
+}
+
+fn verify_input_file(filename: &str) -> Result<String, &'static str>{
+    if Path::new(filename).exists() {
+        // 使用 into 来进行类型转化，只要filename 实现了 Display trait 就可以
+        Ok(filename.into())
+    } else {
+        Err("file does not exist")
+    }
 }
