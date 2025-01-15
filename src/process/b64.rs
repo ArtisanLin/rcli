7c9530906983b9da508e3dc99f3138a0a93075ad
陈定涛 <dingtaochen@zhongkehuizhou.com>
2025-01-16 06:01:23 +0800

    feat: 【base64】使用Box处理不同的返回类型的兼容

--- src/process/b64.rs
+++ src/process/b64.rs
@@ -1,17 +1,19 @@
-use std::fs::File;
+use std::{fs::File, io::Read};
 use base64::{engine::general_purpose::URL_SAFE, Engine as _};
 use crate::Base64Format;
 
 pub fn process_encode(input: &str, format: Base64Format) -> anyhow::Result<()> {
-    // NOTE: 处理不同的数据类型
-    let reader = if input == "-" {
+    // NOTE: 处理不同的数据类型, 将不同的类型提升到一个 dynamic trait 下
+    // NOTE: dyn 有什么作用？
+    // NOTE: 为什么需要Box包裹一层？
+    let reader: Box<dyn Read> = if input == "-" {
         // 返回 Stdin
-        std::io::stdin()
+        Box::new(std::io::stdin())
     } else {
         // 返回 File 类型
-        File::open(input)?
+        Box::new(File::open(input)?)
     };
-    
+
     let encoded = URL_SAFE.encode(input);
     println!("{}", encoded);
     Ok(())
